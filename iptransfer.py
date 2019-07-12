0d70f9996b18a87961c68fb268c420451f37a6f0
Bosdami <46033232+Bosdami@users.noreply.github.com>
2019-07-12 18:40:19 +0100

    Add files via upload

---
+++ iptransfer.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  1 11:47:37 2019
+
+@author: Bosola Omisore
+"""
+import json
+import radix
+import pandas as pd
+filename = open('blackip.txt', "r")# This opens the blacklist
+Blacklist = []#creates a list of blacklist
+for line in filename:
+    Blacklist.append(line.strip().split('\n'))#removes the \n from the Ip addresses
+
+blist = [(x) for [x] in Blacklist]#converting it to a list
+
+with open ('list_of_ip.json','r') as t_list:#loads the json file
+    d = json.load(t_list)
+
+
+
+address1 = [item['original_block'] for item in d]#selecting an item in the transfer
+address = set(address1)
+
+
+rtree = radix.Radix()
+rnode = []
+
+for a in address:
+    rnode.append(rtree.add(a))
+err =[]
+for q in blist:
+
+    err.append(rtree.search_best(q))
+    print(err)
+
+#df = pd.DataFrame(err)
+  #  df.to_csv(r'C:\Users\RILGRAIN\export_dataframe.csv', index= False)
+print 
+
+    
+ #   pd.to_csv('result.csv')
+#    print(err)
+
+#for x in rnode:
+#    print (x)
+
+#err = []
+#for (r,b) in zip(rnode, blist):
+#for b in blist:
+   # err.append(rtree.search_best(b))    
+
+#print (err)
+
