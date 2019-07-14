596154eafb71834ccfd702294cab33d0fe012a35
Ubuntu <ubuntu@ip-172-31-29-39.ec2.internal>
2019-07-14 14:31:29 +0000

    Fixed issues with redundant code

--- iptransfer.py
+++ iptransfer.py
@@ -7,30 +7,19 @@ Created on Thu Jul  1 11:47:37 2019
 import json
 import radix
 import pandas as pd
-filename = open('blackip.txt', "r")# This opens the blacklist
-Blacklist = []#creates a list of blacklist
+filename = open('blackip.txt', "r")  # This opens the blacklist
+blacklist = []  #creates a list of blacklist
 for line in filename:
-    Blacklist.append(line.strip().split('\n'))#removes the \n from the Ip addresses
-
-blist = [(x) for [x] in Blacklist]#converting it to a list
-
-with open ('list_of_ip.json','r') as t_list:#loads the json file
-    d = json.load(t_list)
-
-
-
-address1 = [item['original_block'] for item in d]#selecting an item in the transfer
-address = set(address1)
-
+    blacklist.append(line.strip())
 
 rtree = radix.Radix()
-rnode = []
+with open ('list_of_ip.json','r') as t_list:#loads the json file
+    d = json.load(t_list)
+    for item in d:
+        rnode = rtree.add(item['original_block'])
 
-for a in address:
-    rnode.append(rtree.add(a))
 err =[]
-for q in blist:
-
+for q in blacklist:
     err.append(rtree.search_best(q))
 print(err)
 
