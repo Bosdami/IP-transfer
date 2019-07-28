069ae547e8a6de49ff7246d8396a3a4cd731c9ae
vgiotsas <giotsas@hotmail.com>
2019-07-29 02:35:39 +0300

    Fixed date parsing

--- iptransfer1.py
+++ iptransfer1.py
@@ -5,32 +5,31 @@ Created on Fri Jul 26 18:08:42 2019
 @author: Bosola Omisore
 """
 
+import gzip
 import json
 import radix
-
-filename = open('blacklisted.txt', "r")  # This opens the blacklist
-blacklist = []  #creates a list of blacklist
-for line in filename:
-    blacklist.append(line.strip())
-     
-
-black = []
-for element in blacklist:
-    black.append(element.split(','))
-
-newblack = dict(black)
+from datetime import datetime
+from datetime import timedelta
 
 rtree = radix.Radix()
 with open ('list_of_ip.json','r') as t_list:#loads the json file
     d = json.load(t_list)
     for item in d:
-        rnode = rtree.add(item['original_block'])
+        for ip in item['transferred_blocks'].split(","):
+            rnode = rtree.add(ip.strip())
+            rnode.data['date'] = item['date']
 
+with gzip.open('blacklisted_ips_dates.txt.gz', "rt") as fin:
+    blacklist = set()  # creates a list of blacklist
+    for line in fin:
+        lf = line.strip().split(",")
+        if len(lf) > 1:
+            ip = lf[0]
+            # blacklist.append(ip)
+            rnode = rtree.search_best(ip)
+            if rnode is not None:
+                transfer_dt = datetime.strptime(rnode.data['date'], '%d/%m/%Y')
+                blacklist_dt = datetime.strptime(lf[1], '%Y-%m-%d')
+                elapsed_time = abs((transfer_dt - blacklist_dt).total_seconds() / 3600) # Elapsed time in hours
+                print(ip, elapsed_time)
 
-
-err =[]
-for q in newblack:
-    err.append(rtree.search_best(q))
-
-
-print(err)#prints transferred addresses in the blacklist
